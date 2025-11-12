3a512784300fd8b456b3907864d08e300ecd1b46
Henrique Schroeder <henriqueschroederhs@gmail.com>
2025-11-12 00:12:42 -0300

    start react

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-
-// Criar instância do router
-const router = createRouter({ routeTree })
-
-// Registrar o router para inferência de tipos
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-// Renderizar a aplicação
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
